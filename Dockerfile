408d7d0fd7bf404d262050a355bb51e89132c16d
Preston Massey-Blake <pmassey-blake@mail.csuchico.edu>
2022-11-04 20:31:51 -0700

    finishing touches

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 FROM node:16
-WORKDIR /liatro
+WORKDIR /liatrio
 COPY package.json index.js index.html index.css ./
 RUN npm install 
 EXPOSE 80
 CMD ["npm","start"]  
-#docker build -t liatro_latest .
-# docker run -d -p 80:80 --name node-app liatro_latest 
+#docker build -t liatrio_latest .
+# docker run -d -p 80:80 --name node-app liatrio_latest 
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
-const fs = require('fs/promises');
-const express = require('express');
-const cors = require('cors');
+const fs = require("fs/promises");
+const express = require("express");
+const cors = require("cors");
 const _ = require("lodash");
 const path = require("path");
 const { v4: uuid } = require("uuid");
 const port = 80;
-const hostname = "localhost";
 const app = express();
-
 
 app.get("/about", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
@@ -15,24 +13,22 @@ app.get("/about", function (req, res) {
 
 // Simple Api requesting the basic information
 
-var name = "Preston"
-const facts = ["I won a state championship in high school for Rowing",
-    "My dog has survived cancer 3 times (hes a fighter!)",
-    "I ran my own web design business for 2 years when I started coding",
-    "I once shaved my eyebrow for a dare and it did not grow back for a year",
-"I am a Portugese citizen as well as a citizen in America"]
+var name = "Preston";
+const facts = [
+  "I won a state championship in high school for Rowing",
+  "My dog has survived cancer 3 times (hes a fighter!)",
+  "I ran a web design business for 2 years with some friends.",
+  "I once shaved my eyebrow for a dare and it did not grow back for a year",
+  "I am a Portugese citizen as well as a citizen in America",
+];
 app.get("/", (req, res) => {
-    let idx = Math.floor(Math.random() * facts.length - 1);
-    console.log(idx);
+  let temp_rand = Math.random();
+  let idx = Math.floor(temp_rand * facts.length);
   res.json({
-      "message": `My name is ${name}`,
-      "funFact": facts[idx],
-    "timestamp": new Date().getTime(),
-    
-    
-
+    message: `My name is ${name}`,
+    funFact: facts[idx],
+    timestamp: new Date().getTime(),
   });
 });
-
 
 app.listen(port);
